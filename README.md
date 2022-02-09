d2d4e04fd4841508924502a53dddbe33a8c41316
4tolon <ferzapico@gmail.com>
2022-02-09 21:01:28 +0100

    actualizo 👍

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # numpyexploration
+
+![](img/numpylogo.svg)
+
+https://numpy.org/doc/stable/index.html
+
+
---
+++ scr/numpy.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+a = [1,2,3,4,5]
+print(a)
+print(a[1])
+
+b = np.array([1,2,3,4,5])
+print(b)
+print(b[1])
